792812f866b1d9713a4f44599bf473eacc4d4af0
h4rdw1r3 <vinaugust6@gmail.com>
2020-06-12 15:44:46 +0530

    setup admob

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
+import 'package:admob_flutter/admob_flutter.dart';
 import 'package:flutter/material.dart';
 import 'package:freeschool_mobile/category/categories_page.dart';
 import 'package:get/get.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  Admob.initialize("ca-app-pub-2582751373548446~2091118435");
+  
   runApp(MyApp());
 }
 
