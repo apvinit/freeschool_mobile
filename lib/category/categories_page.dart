d3d3e970030c50565e93fbbab37ba75f4c8e441f
h4rdw1r3 <vinaugust6@gmail.com>
2020-06-24 15:16:46 +0530

    update category page layout

--- lib/category/categories_page.dart
+++ lib/category/categories_page.dart
@@ -1,67 +1,58 @@
-import 'package:admob_flutter/admob_flutter.dart';
 import 'package:flutter/material.dart';
 import 'package:freeschool_mobile/category/category.dart';
 import 'package:freeschool_mobile/course/courses_page.dart';
-import 'package:freeschool_mobile/services/ads.dart';
 import 'package:freeschool_mobile/services/remote.dart';
-import 'package:freeschool_mobile/widgets/custom_divider.dart';
 import 'package:get/get.dart';
 
 class CategoryPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: Text('Free School'),
-        ),
-        body: CourseCategoryList());
+    return Scaffold(body: SafeArea(child: CourseCategoryList()));
   }
 }
 
 class CourseCategoryList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder<List<Category>>(
-      future: getCategories(),
-      builder: (context, snapshot) {
-        if (snapshot.hasData) {
-          var categories = snapshot.data;
-          return ListView.separated(
-              itemCount: categories.length,
-              itemBuilder: (_, index) {
-                if (index != 0 && index % 3 == 0) {
-                  return Column(
-                    children: [
-                      SizedBox(height: 8),
-                      Stack(
-                        alignment: Alignment.center,
-                        children: [
-                          Center(child: Text('Ad')),
-                          AdmobBanner(
-                            adSize: AdmobBannerSize.LARGE_BANNER,
-                            adUnitId: getCategoryPageBannerId(),
-                          ),
-                        ],
-                      ),
-                      Divider(indent: 16),
-                      CourseCategoryTile(
-                        category: categories[index],
-                      )
-                    ],
-                  );
-                }
-                return CourseCategoryTile(
-                  category: categories[index],
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Container(
+          padding: EdgeInsets.all(16),
+          child: Text(
+            'Subjects',
+            style: Theme.of(context).textTheme.headline2.copyWith(
+                  fontWeight: FontWeight.w900,
+                ),
+          ),
+        ),
+        Expanded(
+          child: FutureBuilder<List<Category>>(
+            future: getCategories(),
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                var categories = snapshot.data;
+                return GridView.builder(
+                  padding: EdgeInsets.all(8.0),
+                  itemCount: categories.length,
+                  gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+                      maxCrossAxisExtent: 200, childAspectRatio: 0.95),
+                  itemBuilder: (_, index) {
+                    return CourseCategoryTile(
+                      category: categories[index],
+                    );
+                  },
                 );
-              },
-              separatorBuilder: (_, __) => CustomDivider());
-        } else if (snapshot.hasError) {
-          return Center(
-            child: Text("error occured"),
-          );
-        }
-        return Center(child: CircularProgressIndicator());
-      },
+              } else if (snapshot.hasError) {
+                return Center(
+                  child: Text("error occured"),
+                );
+              }
+              return Center(child: CircularProgressIndicator());
+            },
+          ),
+        ),
+      ],
     );
   }
 }
@@ -73,22 +64,31 @@ class CourseCategoryTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-      contentPadding: EdgeInsets.all(8),
-      leading: CircleAvatar(
-        radius: 24,
-        backgroundColor: Colors.grey[100],
-        backgroundImage: NetworkImage(getMediaUrl(category.cover)),
+    return Card(
+      child: InkWell(
+        onTap: () {
+          Get.to(CoursesPage(
+            categoryID: category.id,
+          ));
+        },
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Image.network(
+              getMediaUrl(category.cover),
+              height: 150,
+              fit: BoxFit.cover,
+            ),
+            Expanded(
+              child: Text(
+                category.title,
+                style: Theme.of(context).textTheme.headline6,
+                textAlign: TextAlign.center,
+              ),
+            ),
+          ],
+        ),
       ),
-      title: Text(
-        category.title,
-        style: TextStyle(fontWeight: FontWeight.w600),
-      ),
-      onTap: () {
-        Get.to(CoursesPage(
-          categoryID: category.id,
-        ));
-      },
     );
   }
 }
